ad7280b2d5a363d0bd0ff5b83de95b46b99564cf
pelle <pelle>
2003-11-06 23:48:05 +0000

    Major Refactoring of PaymentProcessor. Factored out AssetController to be new abstract parent class together with most of its support classes. Created (Half way) RemoteAssetController, which can perform transactions on external AssetControllers via NeuClear. Created the first attempt at the ExchangeAgent. This will need use of the RemoteAssetController. SOAPTools was changed to return a stream. This is required by the VerifyingReader in NeuClear.

--- src/java/org/neuclear/ledger/PostedHeldTransaction.java
+++ src/java/org/neuclear/ledger/PostedHeldTransaction.java
@@ -4,7 +4,6 @@ import java.util.Date;
 import java.util.Iterator;
 
 /**
- * 
  * User: pelleb
  * Date: Jul 29, 2003
  * Time: 3:17:16 PM
@@ -12,22 +11,25 @@ import java.util.Iterator;
 public class PostedHeldTransaction extends PostedTransaction implements HeldTransaction {
     public PostedHeldTransaction(UnPostedHeldTransaction orig, String xid) throws InvalidTransactionException {
         super(orig, xid);
-        this.expiryTime=orig.getExpiryTime();
+        this.expiryTime = orig.getExpiryTime();
     }
+
     public Date getExpiryTime() {
         return expiryTime;
     }
+
     final private Date expiryTime;
 
     /**
-   * Implements a held transfer
-   * @param actualAmount
-   * @param transactionTime
-   * @param comment
-   * @return New Version
-   */
-    public PostedTransaction complete(double actualAmount,Date transactionTime,String comment) throws TransactionExpiredException, InvalidTransactionException, LowlevelLedgerException {
-        return getLedger().performCompleteHold(this,actualAmount,transactionTime,comment);
+     * Implements a held asset
+     * 
+     * @param actualAmount    
+     * @param transactionTime 
+     * @param comment         
+     * @return New Version
+     */
+    public PostedTransaction complete(double actualAmount, Date transactionTime, String comment) throws TransactionExpiredException, InvalidTransactionException, LowlevelLedgerException {
+        return getLedger().performCompleteHold(this, actualAmount, transactionTime, comment);
     }
 
     public void cancel() throws UnknownTransactionException, LowlevelLedgerException {
@@ -35,13 +37,13 @@ public class PostedHeldTransaction extends PostedTransaction implements HeldTran
     }
 
     PostedTransaction reverse(String comment) throws InvalidTransactionException, UnBalancedTransactionException, LowlevelLedgerException {
-         UnPostedHeldTransaction reverse=new UnPostedHeldTransaction(getLedger(),"REVERSAL of "+getXid(),getTransactionTime(),getExpiryTime(),false);
-         Iterator iter=getItems();
-         while (iter.hasNext()){
-             TransactionItem item=(TransactionItem)iter.next();
-             reverse.addItem(item.getBook(),-item.getAmount());
-         }
-         return reverse.post();
-     }
+        UnPostedHeldTransaction reverse = new UnPostedHeldTransaction(getLedger(), "REVERSAL of " + getXid(), getTransactionTime(), getExpiryTime(), false);
+        Iterator iter = getItems();
+        while (iter.hasNext()) {
+            TransactionItem item = (TransactionItem) iter.next();
+            reverse.addItem(item.getBook(), -item.getAmount());
+        }
+        return reverse.post();
+    }
 
 }
